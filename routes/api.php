a617b1c034ea8a6178e6e33456bbdbf9919a0c40
KYOJIDUC <kyojitachabana@gmail.com>
2020-12-21 23:35:39 +0700

    Create API Rest with Laravel 8.X Passport Authentication part3 finish

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
+Route::post('refreshtoken', [UserController::class, 'refreshToken']);
 
 
 Route::get('/unauthorized',  [UserController::class, 'unauthorized']);
